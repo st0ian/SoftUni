1332878d3f7edeaaf0e9a1e8d61713a8c6544dd8
Stoyangit config --global user.email stoian2010@abv.bggit config --global core.autocrlf truegit config --global user.name Stoyan <stoian2010@abv.bg>
2022-05-08 19:10:40 +0300

    rename projects

---
+++ DemoApp/E2_P2_BonusScore/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int score = int.Parse(Console.ReadLine());
+        
+        double bonus = 0.0;
+                
+        if (score <= 100)
+        {
+            bonus=5;
+        }
+        else if(score>100 && score<1000)
+        {
+            bonus=0.2*score;
+        }
+        else
+        {
+            bonus = 0.1 * score;
+        }
+
+        int additionalBonus=0;
+
+        if (score%2 == 0)
+        {
+            additionalBonus=1;
+        }
+        else if (score%10 ==5)
+        {
+            additionalBonus = 2;
+        }
+
+        Console.WriteLine(bonus + additionalBonus);
+        Console.WriteLine(score + bonus + additionalBonus);
+    }
+}
---
+++ DemoApp/E3_P3_Time+15 Minutes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int hours = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+
+        int after15min = 15;
+
+        if (minutes + after15min >60)
+        {
+            hours +=1;
+            minutes = minutes + after15min - 60;
+        }
+        else if (minutes + after15min <60)
+        {
+            minutes = minutes + after15min;
+        }
+        else
+        {
+            hours += 1;
+            minutes = 0;
+        }
+
+        if (hours >= 24 && minutes < 10)
+        {
+            Console.WriteLine($"{hours - 24}:0{minutes}");
+        }            
+        else if (hours >=24 && minutes >= 10)
+        {
+            Console.WriteLine($"{hours - 24}:{minutes}");
+        }
+        else if (hours < 24 && minutes < 10)
+        {
+            Console.WriteLine($"{hours}:0{minutes}");
+        }
+        else
+        {
+            Console.WriteLine($"{hours}:{minutes}");
+        }
+        Console.ReadLine();
+
+    }
+}
