ffeb8c761f6a049061a0b59b0472134bfa307186
Stoyangit config --global user.email stoian2010@abv.bggit config --global core.autocrlf truegit config --global user.name Stoyan <stoian2010@abv.bg>
2022-06-07 17:24:24 +0300

    add last from While

--- CSharp_Basics/While Loop - Exercise/P6_Cake/Program.cs
+++ CSharp_Basics/While Loop - Exercise/P6_Cake/Program.cs
@@ -6,10 +6,36 @@ namespace P6_Cake
     {
         static void Main(string[] args)
         {
-            double change = double.Parse(Console.ReadLine())/100;
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"{Math.Round(change, 4):f5}");
-            Console.WriteLine(change);
+            int piecesTotal = width * length;
+
+            string input = Console.ReadLine();
+            int piecesNeeded = 0;
+            bool stop = true;
+
+            while (input != "STOP")
+            {                
+                int piecesNext = int.Parse(input);
+
+                piecesTotal -= piecesNext;
+                if (piecesTotal <= 0)
+                {
+                    piecesNeeded = Math.Abs(piecesTotal);
+                    stop = false;
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (!stop)
+            {
+                Console.WriteLine($"No more cake left! You need {piecesNeeded} pieces more.");
+            }
+            else
+            {
+                Console.WriteLine($"{piecesTotal} pieces are left.");
+            }
         }
     }
 }
---
+++ CSharp_Basics/While Loop - Exercise/P7_Moving/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P7_Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int homeVolume = width * length * height;
+
+            int neededVolume = 0;
+            bool done = true;
+
+            string input = Console.ReadLine();
+
+            while (input != "Done")
+            {
+                int nextBoxes = int.Parse(input);
+
+                homeVolume -= nextBoxes;
+
+                if (homeVolume <= 0)
+                {
+                    neededVolume = Math.Abs(homeVolume);
+                    done = false;
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            
+            if (done)
+            {
+                Console.WriteLine($"{homeVolume} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {neededVolume} Cubic meters more.");
+            }
+        }
+    }
+}
