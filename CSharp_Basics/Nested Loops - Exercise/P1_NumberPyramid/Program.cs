634b0e565b486ef52993306a28ce42ccf2966f99
Stoyangit config --global user.email stoian2010@abv.bggit config --global core.autocrlf truegit config --global user.name Stoyan <stoian2010@abv.bg>
2022-06-12 18:21:53 +0300

    nested loops exercise

---
+++ CSharp_Basics/Nested Loops - Exercise/P1_NumberPyramid/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P1_NumberPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int curNum = 1;
+
+            for (int row = 1; row <= n; row++)
+            {                
+                bool stop = false;
+
+                for (int numOnRow = 1; numOnRow <= row; numOnRow++)
+                {
+                    Console.Write($"{curNum} ");
+                    curNum++;
+
+                    if (curNum > n)
+                    {
+                        stop = true;
+                        break;
+                    }
+                }
+                if (stop)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CSharp_Basics/Nested Loops - Exercise/P2_EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P2_EqualSumsEvenOddPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            for (int start = first; start <= second; start++)
+            {
+                string curNum = start.ToString();
+                int sumOdd = 0;
+                int sumEven = 0;
+
+                for (int position = 0; position < curNum.Length; position++)
+                {
+                    int digit = int.Parse(curNum[position].ToString());
+                    if (position%2 == 0)
+                    {
+                        sumEven += digit;
+                    }
+                    else
+                    {
+                        sumOdd += digit;
+                    }
+                }
+                if (sumEven == sumOdd)
+                {
+                    Console.Write($"{curNum} ");
+                }
+            }
+        }
+    }
+}
