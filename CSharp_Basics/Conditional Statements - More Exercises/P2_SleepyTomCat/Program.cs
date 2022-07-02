9038a30d2bb469ee656e120cfefc421abd847bb5
Stoyangit config --global user.email stoian2010@abv.bggit config --global core.autocrlf truegit config --global user.name Stoyan <stoian2010@abv.bg>
2022-07-02 14:30:58 +0300

    Conditional Statements - More Exercises

---
+++ CSharp_Basics/Conditional Statements - More Exercises/P2_SleepyTomCat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P2_SleepyTomCat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOff = int.Parse(Console.ReadLine());
+
+            int neededSleepMinutes = 30000;
+
+            int playTime = daysOff * 127 + (365 - daysOff) * 63;
+
+            int hoursDiff = Math.Abs((neededSleepMinutes - playTime) / 60);
+            int minutesDiff = Math.Abs((neededSleepMinutes - playTime) % 60);
+            
+            if (neededSleepMinutes >= playTime)
+            {
+                Console.WriteLine($"Tom sleeps well\n{hoursDiff} hours and {minutesDiff} minutes less for play");
+            }
+            else
+            {
+                Console.WriteLine($"Tom will run away\n{hoursDiff} hours and {minutesDiff} minutes more for play");
+            }
+        }
+    }
+}
---
+++ CSharp_Basics/Conditional Statements - More Exercises/P3_Harvest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P3_Harvest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int area = int.Parse(Console.ReadLine());
+            double yeildPerSqM = double.Parse(Console.ReadLine());
+            int neededLiters = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+
+            double totalYeild = area * yeildPerSqM;
+            double yeildForWine = 0.40 * totalYeild;
+
+            double producedLiters = yeildForWine / 2.5;
+
+            if (producedLiters >= neededLiters)
+            {
+                double difference = producedLiters - neededLiters;
+                double littersPerWorker = Math.Ceiling(difference / workers);
+
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(producedLiters)} liters.\n{Math.Ceiling(difference)} liters left -> " +
+                    $"{Math.Ceiling(littersPerWorker)} liters per person.");
+            }
+            else
+            {
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(neededLiters-producedLiters)} liters wine needed.");
+            }
+        }
+    }
+}
