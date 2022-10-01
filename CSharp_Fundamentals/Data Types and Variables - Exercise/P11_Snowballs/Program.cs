e9f429e8e286ee4d2eaaf46adedad185b9490522
Stoyangit config --global user.email stoian2010@abv.bggit config --global core.autocrlf truegit config --global user.name Stoyan <stoian2010@abv.bg>
2022-10-01 09:13:40 +0300

    add tasks to Basic Syntax, Conditional Statements and Loops - Exercise

---
+++ CSharp_Fundamentals/Data Types and Variables - Exercise/P11_Snowballs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace P11_Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int balls = int.Parse(Console.ReadLine());
+
+            int snowBallSnow = 0;
+            int snowBallTime = 0;
+            int snowBalQualirty = 0;
+            BigInteger snowballValue = BigInteger.MinusOne;
+
+            for (int i = 0; i < balls; i++)
+            {
+                int tempSnowBallSnow = int.Parse(Console.ReadLine());
+                int tempSsnowBallTime = int.Parse(Console.ReadLine());
+                int tempSnowBalQualirty = int.Parse(Console.ReadLine());
+
+                BigInteger tempSnowBallValue = BigInteger.Pow(tempSnowBallSnow / tempSsnowBallTime, tempSnowBalQualirty);
+
+                if (tempSnowBallValue > snowballValue)
+                {
+                    snowballValue = tempSnowBallValue;
+                    snowBallSnow = tempSnowBallSnow;
+                    snowBallTime = tempSsnowBallTime;
+                    snowBalQualirty = tempSnowBalQualirty;
+                }
+            }
+
+            Console.WriteLine($"{snowBallSnow} : {snowBallTime} = {snowballValue} ({snowBalQualirty})");
+        }
+    }
+}
