9ffe15601917dce41a677dba349917c82befcc64
Stoyangit config --global user.email stoian2010@abv.bggit config --global core.autocrlf truegit config --global user.name Stoyan <stoian2010@abv.bg>
2022-11-16 12:02:15 +0200

    finished with Dictionaries - Exercises

--- CSharp_Fundamentals/Associative Arrays-Exercise/P06_StudentAcademy/Program.cs
+++ CSharp_Fundamentals/Associative Arrays-Exercise/P06_StudentAcademy/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace P06_StudentAcademy
 {
@@ -7,9 +8,30 @@ namespace P06_StudentAcademy
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> see = new Dictionary<string, string>();
+            Dictionary<string, List <double>> studentsBook = new Dictionary<string, List<double>>();
 
-            Console.WriteLine("Hello World!");
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!studentsBook.ContainsKey(name))
+                {
+                    studentsBook.Add(name, new List<double>());
+                }
+
+                studentsBook[name].Add(grade);      
+            }
+
+            foreach (var kvp in studentsBook)
+            {
+                if (kvp.Value.Average() >= 4.5)
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value.Average():f2}");
+                }                
+            }
         }
     }
 }
---
+++ CSharp_Fundamentals/Associative Arrays-Exercise/P07_CompanyUsers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P07_CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companyRegister = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputArgs = input.Split(" -> ");
+                string companyName = inputArgs[0];
+                string employeName = inputArgs[1];
+
+                if (!companyRegister.ContainsKey(companyName))
+                {
+                    companyRegister.Add(companyName, new List<string>());
+                }
+                if (!companyRegister[companyName].Contains(employeName))
+                {
+                    companyRegister[companyName].Add(employeName);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var company in companyRegister)
+            {
+                Console.WriteLine($"{company.Key}");
+                foreach (var id in company.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }                
+            }
+        }
+    }
+}
